3b087257d63b3e132f7b7cd80177482a55d361eb
Taiwo Adepoju <tadepoju95@gmail.com>
2020-09-08 05:00:32 -0500

    Modified all files in project folder.

--- rpsgame.js
+++ rpsgame.js
@@ -1,29 +1,45 @@
 let rock = () => {
-  let arrayOfElements = ['paper', 'scissors'];
+  let arrayOfElements = ['paper', 'scissors', 'rock'];
   let randomItems = Math.floor(Math.random() * arrayOfElements.length);
   if(randomItems === arrayOfElements.indexOf('paper')) {
-    document.getElementById('setWinnerOrLoser').innerHTML ='Computer wins!';
+    document.getElementById('setWinnerOrLoser').innerHTML ='Computer wins!!!';
+    document.getElementById('getComputerInput').innerHTML = 'Computer played paper';
   } else if(randomItems === arrayOfElements.indexOf('scissors')) {
-    document.getElementById('setWinnerOrLoser').innerHTML ='You win!';
+    document.getElementById('setWinnerOrLoser').innerHTML ='You win!!!';
+    document.getElementById('getComputerInput').innerHTML = 'Computer played scissors';
+  } else if(randomItems === arrayOfElements.indexOf('rock')) {
+    document.getElementById('setWinnerOrLoser').innerHTML ="It's a tie!!!";
+    document.getElementById('getComputerInput').innerHTML = 'Computer played rock';
   }
 }
 
+
 let paper = () => {
-  let arrayOfElements = ['rock', 'scissors'];
+  let arrayOfElements = ['rock', 'scissors', 'paper'];
   let randomItems = Math.floor(Math.random() * arrayOfElements.length);
   if(randomItems === arrayOfElements.indexOf('rock')) {
     document.getElementById('setWinnerOrLoser').innerHTML ='You win!';
+    document.getElementById('getComputerInput').innerHTML = 'Computer played rock';
   } else if(randomItems === arrayOfElements.indexOf('scissors')) {
     document.getElementById('setWinnerOrLoser').innerHTML ='Computer wins!';
+    document.getElementById('getComputerInput').innerHTML = 'Computer played scissors';
+  } else if(randomItems === arrayOfElements.indexOf('paper')) {
+    document.getElementById('setWinnerOrLoser').innerHTML ="It's a tie!!!";
+    document.getElementById('getComputerInput').innerHTML = 'Computer played paper';
   }
 }
 
 let scissors = () => {
-  let arrayOfElements = ['rock', 'paper'];
+  let arrayOfElements = ['rock', 'paper', 'scissors'];
   let randomItems = Math.floor(Math.random() * arrayOfElements.length);
   if(randomItems === arrayOfElements.indexOf('rock')) {
     document.getElementById('setWinnerOrLoser').innerHTML ='Computer wins!';
+    document.getElementById('getComputerInput').innerHTML = 'Computer played rock';
   } else if(randomItems === arrayOfElements.indexOf('paper')) {
     document.getElementById('setWinnerOrLoser').innerHTML ='You win!';
+    document.getElementById('getComputerInput').innerHTML = 'Computer played paper';
+  } else if(randomItems === arrayOfElements.indexOf('scissors')) {
+    document.getElementById('setWinnerOrLoser').innerHTML ="It's a tie!!!";
+    document.getElementById('getComputerInput').innerHTML = 'Computer played scissors';
   }
 }
